2ce8744f216b706d7199a27cf168a4615141c149
raksh543 <rakshitajain777@gmail.com>
2020-08-05 11:25:19 +0530

    first commit

---
+++ src/components/Accordion.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const ACCordion = ({ items }) => {
+    const onTitleClick = (index) => {
+        console.log(index)
+    }
+
+    const renderedItems = items.map((item, index) => {
+        return(
+            <React.Fragment key={item.title}>
+                <div onClick={()=>onTitleClick(index)} className="title active">
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className="content active">
+                    <p>{item.content}</p>
+                </div>
+            </React.Fragment>
+        )
+    });
+
+    return(
+        <div className="ui styled accordion">
+            {renderedItems}
+        </div>
+    )
+};
+
+export default ACCordion;
