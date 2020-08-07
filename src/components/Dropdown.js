29dcc4b794c867fadf96a4af34af7b084be8687d
raksh543 <rakshitajain777@gmail.com>
2020-08-07 21:53:26 +0530

    adding toogle to dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Dropdown = ({options, selected, onSelectedChange}) => {
+    const [open, setOpen] = useState(false);
 
     const renderedOptions = options.map((option)=>{
+        if (option.value === selected.value){
+            return null;
+        }
         return(
             <div key={option.value} className="item" onClick={() => onSelectedChange(option)}>
                 {option.label}
@@ -15,10 +19,10 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
                 <div className="label">
                     Select a color
                 </div>
-                <div className="ui selection dropdown visible active">
+                <div onClick={()=>setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
-                    <div className="menu visible transition">
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>
                         {renderedOptions}
                     </div>
                 </div>
