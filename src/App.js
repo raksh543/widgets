0b248bd76a61ebcd75d3f5a576a299f65e7fe429
raksh543 <rakshitajain777@gmail.com>
2020-08-07 21:43:26 +0530

    initiating dropdown widget

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items=[
     {
@@ -17,11 +18,31 @@ const items=[
     }
 ]
 
+const options = [
+    {
+        label: 'Red',
+        value: 'red'
+    },{
+        label:'Green',
+        value: 'green'
+    },{
+        label:'A shade of blue',
+        value: 'blue'
+    }
+]
+
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return(
         <div className="ui container">
             {/* <Accordion items={items}/> */}
-            <Search />
+            {/* <Search /> */}
+            <Dropdown
+                selected={selected}
+                onSelectedChange={setSelected}
+                options={options}
+            />
         </div>
     )
 };
