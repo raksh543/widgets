6e0b1d8ef0f2e710f65ff318fab1082916d013cf
raksh543 <rakshitajain777@gmail.com>
2020-08-08 09:39:45 +0530

    using useRef to close dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({options, selected, onSelectedChange}) => {
     const [open, setOpen] = useState(false);
+
+    const ref = useRef();
+
+    useEffect(()=>{
+        document.body.addEventListener('click',(e)=>{
+            // console.log(e.target)
+            // console.log('CLICK!!!')
+            if(ref.current.contains(e.target)){
+                return;
+            }
+            setOpen(false)
+        });
+    },[]);
 
     const renderedOptions = options.map((option)=>{
         if (option.value === selected.value){
@@ -13,13 +26,19 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
             </div>
         );
     });
+
+    // console.log(ref.current);
+
     return(
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <div className="label">
                     Select a color
                 </div>
-                <div onClick={()=>setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active' : ''}`}>
+                <div 
+                    onClick={()=>setOpen(!open)} 
+                    className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+                >
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
                     <div className={`menu ${open ? 'visible transition' : ''}`}>
